eb714fd461af2bd6f455011de2375170beacb4ac
JubyThomas <jubyroselinethomas@gmail.com>
2021-07-29 07:30:36 +0100

    changes in chater 5

--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,19 @@
-  
 import  requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    # Refer to the TwD book for more information on how this updated view works.
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+    # Spoiler: you don't need to pass a context dictionary here.
     return render(request, 'rango/about.html')
 
+
+
+    
 def show_movies(request):
 
         result = requests.get('https://api.disneyapi.dev/characters')
